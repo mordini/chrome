b6f4465dd93cb0aeb736ab156cea309d55440fcb
mordini <yahewisthedevil@yahoo.com>
2018-05-14 22:09:26 +0000

    working on JSON handling

--- savesnippets/python/savesnippets.py
+++ savesnippets/python/savesnippets.py
@@ -1,11 +1,28 @@
 # Save Snippets from Google Chrome
 import json
 import re
+from pprint import pprint
 
 # Begin
 def main():
     pref_obj = openFile("Preferences")
-    getSnippets(pref_obj)
+    #getSnippets(pref_obj)
+    testJSON(pref_obj)
+    closeFile(pref_obj)
+
+# TEST GETTING JSON INFORMATION
+def testJSON(pref_obj):
+    data = json.load(pref_obj)
+    snippets = data['devtools']['preferences']['scriptSnippets']
+    #print(snippets)
+    data['devtools']['preferences']['scriptSnippets'] = ''
+    print('New snippets')
+    print(data['devtools']['preferences']['scriptSnippets'])
+    data['devtools']['preferences']['scriptSnippets'] = snippets
+    print('Old snippets')
+    print(data['devtools']['preferences']['scriptSnippets'])
+    #for e in data['devtools']:
+        #print(e)
 
 # Try to open the file
 # Note:  for "easy mode" use "with open" statement
@@ -15,8 +32,9 @@ def openFile(prefName):
     try:
         pref_obj = open(prefName, "r")
         return pref_obj
-    except IOError:
-        print(prefName+' does not exist.')
+    except IOError as e:
+        print e.errno, e.strerror
+        #print(prefName+' does not exist.')
         return
 
 # Close the file
