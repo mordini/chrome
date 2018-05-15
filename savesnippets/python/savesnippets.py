6d651372e4527b9bf68a1394e20844ca504ed925
mordini <yahewisthedevil@yahoo.com>
2018-05-14 22:10:12 -0400

    removed old functions, moved functions around, added parameter for filename

--- savesnippets/python/savesnippets.py
+++ savesnippets/python/savesnippets.py
@@ -21,14 +21,19 @@ def main():
     #test = File('Preferences')
     #print(test.profName)
     #testJSON(pref_obj)
-    pref_obj = getPrefObj()
+    fileName = getFileName()
+    pref_obj = getPrefObj(fileName)
     data = getData(pref_obj)
     writeBackupFile(data)
     closeFile(pref_obj)
 
 #rewrite me to use class/OOP stuff
-def getPrefObj():
-    pref_obj = openFile("Preferences")
+def getFileName():
+    fileName = sys.argv[1]
+    print(sys.argv[1])
+    return fileName
+def getPrefObj(fileName):
+    pref_obj = openFile(fileName)
     return pref_obj
 def getData(pref_obj):
     data = json.load(pref_obj)
@@ -45,22 +50,6 @@ def writeBackupFile(data):
     backupFile = open(profName, 'w')
     backupFile.write(snippets)
     backupFile.close()
-
-# TEST GETTING JSON INFORMATION
-def testJSON(pref_obj):
-    data = json.load(pref_obj)
-    snippets = data['devtools']['preferences']['scriptSnippets']
-    testData = data['profile']['name']
-    data['devtools']['preferences']['scriptSnippets'] = ''
-    print('New snippets')
-    print(data['devtools']['preferences']['scriptSnippets'])
-    data['devtools']['preferences']['scriptSnippets'] = snippets
-    print('Old snippets')
-    print(data['devtools']['preferences']['scriptSnippets'])
-
-    print(testData)
-    for e in testData:
-        print(e)
 
 # Try to open the file
 # Note:  for "easy mode" use "with open" statement
@@ -85,28 +74,25 @@ def getPrefText(pref_obj):
     prefText = pref_obj.read()
     return prefText
 
-# Get the snippets section from Chrome prefs
-def getSnippetsOLD(pref_obj):
-    prefText = getPrefText(pref_obj)
-
-    if prefText:
-        closeFile(pref_obj)
-
-    openTag = 'scriptSnippets'
-    closeTag = '","scriptSnippets_lastIdentifier'
-    #snippets = '{"' + openTag + prefText.split(openTag)[1].split(closeTag)[0]+'}' 
-    #snippets = prefText.split(openTag)[1].split(closeTag)[0]
-    snippets = re.search(openTag+'(.*)'+closeTag, prefText)
-    formatSnippets(snippets.group(1))
-
 # Print the snippet section
 def printFileText(pref_obj, prefText):
     print('showing text')
     print(prefText)
     closeFile(pref_obj)
 
-def formatSnippets(snippets):
-    newLineSnippets = snippets.replace('\\n', '\n').replace('\\','')
-    print(newLineSnippets)
+# TEST GETTING JSON INFORMATION
+def testJSON(pref_obj):
+    data = json.load(pref_obj)
+    snippets = data['devtools']['preferences']['scriptSnippets']
+    testData = data['profile']['name']
+    data['devtools']['preferences']['scriptSnippets'] = ''
+    print('New snippets')
+    print(data['devtools']['preferences']['scriptSnippets'])
+    data['devtools']['preferences']['scriptSnippets'] = snippets
+    print('Old snippets')
+    print(data['devtools']['preferences']['scriptSnippets'])
 
+    print(testData)
+    for e in testData:
+        print(e)
 main()
